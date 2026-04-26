b3da32ecaa70429e64cc7a1cccd36f77d66ae2f6
unicorn5843 <ravlotay5843@googlemail.com>
2026-04-26 08:05:55 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'ecofamilyflow-v2';
-const urlsToCache = [
-  '/Eco-Family-Flow/',
-  '/Eco-Family-Flow/index.html',
-  '/Eco-Family-Flow/manifest.json'
-];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
